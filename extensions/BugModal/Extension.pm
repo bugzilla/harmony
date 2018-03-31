537dfbaf62f7df7322e3d740a6864c6ccc3303a3
Vladimir Panteleev <git@thecybershadow.net>
2018-03-31 04:47:36 +0000

    Use Bugzilla->have_extension
    Transition from previous explicit searches in the extension list to
    invoking the method added in the previous commit.

--- extensions/BugModal/Extension.pm
+++ extensions/BugModal/Extension.pm
@@ -188,8 +188,7 @@ sub template_before_process {
     return if exists $bug->{error};
 
     # trigger loading of tracking flags
-    state $have_tracking_flags = any { $_->NAME eq 'TrackingFlags' } @{ Bugzilla->extensions };
-    if ($have_tracking_flags) {
+    if (Bugzilla->have_extension('TrackingFlags')) {
         Bugzilla::Extension::TrackingFlags->template_before_process({
             file => 'bug/edit.html.tmpl',
             vars => $vars,
