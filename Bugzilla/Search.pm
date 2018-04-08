b0935c1e52eea1dfc70652bf6fcb7c8b856a8826
Vladimir Panteleev <github.private@thecybershadow.net>
2018-04-08 06:57:06 +0300

    Bug 1446236 - Add & use simpler method to check if an extension is present (#35)

--- Bugzilla/Search.pm
+++ Bugzilla/Search.pm
@@ -34,7 +34,6 @@ use Date::Format;
 use Date::Parse;
 use Scalar::Util qw(blessed);
 use List::MoreUtils qw(all firstidx part uniq);
-use List::Util qw(any);
 use POSIX qw(INT_MAX);
 use Storable qw(dclone);
 use Time::HiRes qw(gettimeofday tv_interval);
@@ -803,8 +802,7 @@ sub data {
     # BMO - to avoid massive amounts of joins, if we're selecting a lot of
     # tracking flags, replace them with placeholders. the values will be
     # retrieved later and injected into the result.
-    state $have_tracking_flags = any { $_->NAME eq 'TrackingFlags' } @{ Bugzilla->extensions };
-    if ($have_tracking_flags) {
+    if (Bugzilla->has_extension('TrackingFlags')) {
         my %tf_map = map { $_ => 1 } Bugzilla::Extension::TrackingFlags::Flag->get_all_names();
         my @tf_selected = grep { exists $tf_map{$_} } @orig_fields;
         # mysql has a limit of 61 joins, and we want to avoid massive amounts of joins
@@ -867,7 +865,7 @@ sub data {
     $self->{data} = [map { $data{$_} } @$bug_ids];
 
     # BMO - get tracking flags values, and insert into result
-    if ($have_tracking_flags && @{ $self->{tracking_flags} }) {
+    if (Bugzilla->has_extension('TrackingFlags') && @{ $self->{tracking_flags} }) {
         # read values
         my $values;
         $sql = "
