54f100b70e273f0224919370c11d930c2fa5e5b0
Dylan Hardison <dylan@hardison.net>
2022-12-18 12:48:34 -0800

    Improve error reporting from wrapped DBI methods in Bugzilla::DB

--- Bugzilla/DB.pm
+++ Bugzilla/DB.pm
@@ -67,8 +67,23 @@ around 'attrs' => sub {
     my $symbol = '&' . $method;
     $stash->add_symbol(
       $symbol => sub {
-        my $self = shift;
-        return $self->dbh->$method(@_);
+        my $self      = shift;
+        my $wantarray = wantarray;
+        my $result    = eval {
+          if ($wantarray) {
+            [$self->dbh->$method(@_)];
+          }
+          else {
+            [scalar $self->dbh->$method(@_)];
+          }
+        };
+        if (not defined $result) {
+          my $err = $@;
+          Carp::confess($err);
+        }
+        else {
+          return $wantarray ? @$result : $result->[0];
+        }
       }
     );
   }
@@ -511,8 +526,8 @@ sub bz_server_version {
 sub bz_last_key {
   my ($self, $table, $column) = @_;
 
-  return $self->last_insert_id(Bugzilla->localconfig->db_name,
-    undef, $table, $column);
+  return $self->last_insert_id(Bugzilla->localconfig->db_name, undef, $table,
+    $column);
 }
 
 sub bz_check_regexp {
@@ -579,7 +594,7 @@ sub bz_setup_foreign_keys {
   # so if it doesn't have them, then we're setting up FKs
   # for the first time, and should be quieter about it.
   my $activity_fk = $self->bz_fk_info('profiles_activity', 'userid');
-  my $any_fks = $activity_fk && $activity_fk->{created};
+  my $any_fks     = $activity_fk && $activity_fk->{created};
   if (!$any_fks) {
     print get_text('install_fk_setup'), "\n";
   }
