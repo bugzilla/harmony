1c2e8054f499e65ed5d98211e22a585de619563f
Dave Miller <justdave@bugzilla.org>
2024-03-29 02:17:32 -0400

    let me run pg tests locally

--- docker/run-tests-in-docker.sh
+++ docker/run-tests-in-docker.sh
@@ -51,9 +51,13 @@ export DOCKER_CLI_HINTS=false
 export CI=""
 export CIRCLE_SHA1=""
 export CIRCLE_BUILD_URL=""
-$DOCKER compose -f docker-compose.test.yml build
+DOCKER_COMPOSE_FILE=docker-compose.test.yml
+if [ "$1" == "pg" ]; then
+    DOCKER_COMPOSE_FILE=docker-compose.test-pg.yml
+fi
+$DOCKER compose -f $DOCKER_COMPOSE_FILE build
 if [ $? == 0 ]; then
-    $DOCKER compose -f docker-compose.test.yml run bmo.test test_bmo -q -f t/bmo/*.t
+    $DOCKER compose -f $DOCKER_COMPOSE_FILE run bmo.test test_bmo -q -f t/bmo/*.t
 else
     echo "docker compose build failed."
 fi
